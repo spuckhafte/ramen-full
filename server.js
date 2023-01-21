05b08d2da8de8b69e50cd9231a1fc9990db3d6b8
Rakshit <rakshit240405@gmail.com>
2023-01-21 21:10:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,9 +2,6 @@ import Discord, { Intents, MessageActionRow, MessageButton, MessageEmbed, Messag
 import Details from './secret.js'
 import mongoose from 'mongoose';
 import User from './schema/User.js';
-import http from 'http';
-import { Server } from 'socket.io'
-import { StringDecoder } from 'string_decoder'
 
 import remind from './commands/remind.js';
 import balancePlanning from './commands/balancePlanning.js'
@@ -475,48 +472,3 @@ async function donedonadone() {
 }
 
 client.login(Details.TOKEN)
-
-
-
-
-// ramen vote
-
-const httpServer = http.createServer((req, res) => mainServer(req, res));
-const io = new Server(httpServer, {
-    cors: { origin: "*" }
-});
-
-const PORT = process.env.PORT;
-
-let GlobalSocket = null;
-let BotId = null;
-
-io.on('connection', socket => {
-    console.log(socket.id);
-    socket.on('handshake', botId => {
-      GlobalSocket = socket;
-      BotId = botId;
-      socket.emit('connected', 'ok 200');
-    });
-});
-
-function mainServer(req, res) {
-    if (!GlobalSocket) return;
-    if (req.method == 'POST') {
-        const decoder = new StringDecoder('utf-8');
-        let buffer = '';
-        req.on('data', data => {
-            buffer += decoder.write(data);
-        });
-        req.on('end', () => {
-            buffer += decoder.end();
-            if (JSON.parse(buffer).bot != BotId) return;
-            GlobalSocket.emit(JSON.parse(buffer).type, buffer);
-        });
-        res.end('ok');
-    }
-}
-
-httpServer.listen(PORT, () => {
-    console.log(`listening on ${PORT}*`);
-});
